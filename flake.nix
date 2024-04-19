871711332e83b32a71bc3e68da8c107136d0fb64
luh <lasse@hueffler.de>
2024-04-19 13:49:20 +0200

    test

--- flake.nix
+++ flake.nix
@@ -1,45 +1,31 @@
 {
-  description = "The base config Flake";
+  description = "Nixos config flake";
 
   inputs = {
-    # NixOS official package source, using the unstable branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    helix.url = "github:helix-editor/helix/master";
-
-    hyprland.url = "github:hyprwm/Hyprland";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.config.allowUnfree = true;
+    hosts.url = "./hosts/default";
 
     home-manager = {
       url = "github:nix-community/home-manager";
-
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    #inputs.nixvim = {
-    #  url = "github:nix-community/nixvim";
-    #  # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-    #  # url = "github:nix-community/nixvim/nixos-23.05";
-
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, hosts, ... }@inputs:
+    let
       system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-	{ _module.args = { inherit inputs; }; }
-
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-
-	  home-manager.users.luh = import ./home.nix;
-	}
-      ];
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowUnfree = true; };
+    in
+    {
+      nixosConfigurations = {
+	default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./hosts/default/configuration.nix
+            #inputs.home-manager.nixosModules.default
+          ];
+        };
+      };
     };
-  };
 }
