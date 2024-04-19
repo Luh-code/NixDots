050756add658bad146712b9091356e6697ecb0a8
luh <lasse@hueffler.de>
2024-04-19 18:27:38 +0200

    made alacritty a module

---
+++ modules/home-manager/alacritty/default.nix
@@ -0,0 +1,21 @@
+{ inputs, ... }:
+
+{
+  programs.alacritty = {
+    enable = true;
+
+    settings = {
+      shell.program = "zsh";
+      env.TERM = "xterm-256color";
+      font = {
+        normal = {
+          family = "FiraCode Nerd Font Mono";
+          style = "Regular";
+        };
+        size = 11;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+}
