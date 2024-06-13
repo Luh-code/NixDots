df5ab39e5c4f8052a41fa5844bf1f8b321be876e
luh <lasse@hueffler.de>
2024-06-13 13:00:05 +0200

    fuck this

--- modules/nixos/stylix/default.nix
+++ modules/nixos/stylix/default.nix
@@ -4,7 +4,8 @@
   stylix = {
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-    image = ./backgrounds/illustration-rain-futuristic-city.jpg;
+    #image = ./backgrounds/illustration-rain-futuristic-city.jpg;
+    image = ./1.jpg;
 
     targets.nixos-icons = {
       enable = false;
