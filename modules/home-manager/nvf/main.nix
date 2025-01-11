87a5e5684cfe27d2108a12012bad90bfeadab023
Luh-code <lasse@hueffler.de>
2025-01-11 12:21:22 +0100

    nvf plugins

--- modules/home-manager/nvf/main.nix
+++ modules/home-manager/nvf/main.nix
@@ -1,25 +1,38 @@
 { pkgs, lib, ... }:
 
 {
+  imports = [
+    ./plugins/plugins.nix
+  ];
+
   vim = {
     theme = {
       enable = true;
-      name = "nord";
+      #name = "base16";
       style = "dark";
     };
 
     statusline.lualine.enable = true;
     telescope.enable = true;
-    autocomplete.enable = true;
+    autocomplete.nvim-cmp.enable = true;
 
     languages = {
       enableLSP = true;
       enableTreesitter = true;
 
-      nix.enable = true;
       rust.enable = true;
+      nix.enable = true;
+      sql.enable = true;
+      clang.enable = true;
       ts.enable = true;
+      python.enable = true;
       zig.enable = true;
+      mardown.enable = true;
+      html.enable = true;
+      dart.enable = true;
+      go.enable = true;
+      lua.enable = true;
+      php.enable = true;
     };
   };
 }
---
+++ modules/home-manager/nvf/plugins/aerial.nix
@@ -0,0 +1,33 @@
+{ pkgs, lib, ... }:
+
+{
+  lazy.plugins."aerial.nvim" = { 
+    package = pkgs.vimPlugins.aerial-nvim;
+    setupModule = "aerial";
+    # set up plugin
+    setupOpts = {
+      
+    };
+    # lua initialization after setup
+    after = ''
+      
+    '';
+
+    # Explicitely mark the plugin as lazy. Not strictly required if triggers are defined
+    lazy = true;
+
+    # load on command
+    cmd = ["AerialOpen"];
+
+    # load on event
+    event = ["BufEnter"];
+
+    # load on keymap
+    keys = [
+      {
+        key = "<leader>a";
+        action = ":AerialToggle<CR>";
+      }
+    ];
+  };
+}
